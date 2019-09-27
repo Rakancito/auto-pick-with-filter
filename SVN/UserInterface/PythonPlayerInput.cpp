e3eed532991d15b03d0edd0fc9caaa63b4e27753
Robert <55710848+Rakancito@users.noreply.github.com>
2019-09-27 05:51:43 -0500

    Create PythonPlayerInput.cpp

---
+++ SVN/UserInterface/PythonPlayerInput.cpp
@@ -0,0 +1,28 @@
+// If you don't have a some autopickup Replace 
+
+void CPythonPlayer::PickCloseItem()
+
+//For:
+
+void CPythonPlayer::PickCloseItem()
+{
+	CInstanceBase * pkInstMain = NEW_GetMainActorPtr();
+	if (!pkInstMain)
+		return;
+
+	TPixelPosition kPPosMain;
+	pkInstMain->NEW_GetPixelPosition(&kPPosMain);
+
+	std::vector<DWORD> itemVidList;
+	
+	CPythonItem& rkItem=CPythonItem::Instance();
+	if (!rkItem.GetCloseItemVector(pkInstMain->GetNameString(), kPPosMain, itemVidList))
+		return;
+	
+	if(itemVidList.empty())
+		return;
+	
+	for (int i = 0; i < itemVidList.size(); i++)
+		SendClickItemPacket(itemVidList[i]);
+    
+ }
